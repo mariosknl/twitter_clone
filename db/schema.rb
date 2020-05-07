a7da96b90ddba7a4df81feceb7a84f36a065cca6
mariosknl <mariosknl@outlook.com>
2020-05-07 16:30:22 +0300

    Finished Devise Configuration for User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_102507) do
+ActiveRecord::Schema.define(version: 2020_05_07_120517) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
